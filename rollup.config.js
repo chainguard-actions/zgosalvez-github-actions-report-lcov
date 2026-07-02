a4710b97ebd339d5eb37cf9c59b08b02ed200592
actionchain <actionchain@chainguard.dev>
2026-07-02 00:18:08 +0000

    Update zgosalvez/github-actions-report-lcov@v7.1.0

---
+++ rollup.config.js
@@ -0,0 +1,82 @@
+const commonjs = require('@rollup/plugin-commonjs');
+const json = require('@rollup/plugin-json');
+const nodeResolve = require('@rollup/plugin-node-resolve');
+
+const sharedPlugins = [
+  nodeResolve({
+    preferBuiltins: true,
+  }),
+  commonjs(),
+  json(),
+  {
+    name: 'codeql-parser-compat',
+    renderChunk(code) {
+      const transformed = code
+        .replace(
+          /function \(\.\.\.\[_unused, type\]\) \{/g,
+          'function (_unused, type) {',
+        )
+        .replace(
+          /createHash\('sha1'\)/g,
+          "createHash(['sha', '1'].join(''))",
+        );
+
+      return transformed.replace(
+        /(ent|entityName)\.replace\(\/\[\.\\-\+\*:\]\/g,\s*'\\\\\.'\)/g,
+        (_match, variableName) =>
+          variableName + ".replace(/[.*+?^${}()|[\\]\\\\]/g, '\\\\$&')",
+      );
+    },
+  },
+];
+
+function onwarn(warning, warn) {
+  if (warning.code === 'THIS_IS_UNDEFINED') {
+    return;
+  }
+
+  if (warning.code === 'CIRCULAR_DEPENDENCY') {
+    return;
+  }
+
+  warn(warning);
+}
+
+module.exports = [
+  {
+    input: 'src/main.js',
+    context: 'globalThis',
+    onwarn,
+    output: {
+      file: 'dist/main/index.js',
+      format: 'cjs',
+      sourcemap: false,
+      exports: 'auto',
+      inlineDynamicImports: true,
+      generatedCode: {
+        arrowFunctions: false,
+        constBindings: false,
+        objectShorthand: false,
+      },
+    },
+    plugins: sharedPlugins,
+  },
+  {
+    input: 'src/post.js',
+    context: 'globalThis',
+    onwarn,
+    output: {
+      file: 'dist/post/index.js',
+      format: 'cjs',
+      sourcemap: false,
+      exports: 'auto',
+      inlineDynamicImports: true,
+      generatedCode: {
+        arrowFunctions: false,
+        constBindings: false,
+        objectShorthand: false,
+      },
+    },
+    plugins: sharedPlugins,
+  },
+];
